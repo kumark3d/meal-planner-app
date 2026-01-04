21d12d8f42ecfca49f1d080ce4f6203c30a3e61f
kumark3d <kumark.engineer@gmail.com>
2026-01-03 22:17:14 -0500

    Update generate-meal-plan.js

--- pages/api/generate-meal-plan.js
+++ pages/api/generate-meal-plan.js
@@ -22,8 +22,8 @@ export default async function handler(req, res) {
     }
 
     // Call Gemini API with server-side API key
-    
-    const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${process.env.GEMINI_API_KEY}`, {
+    const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=${process.env.GEMINI_API_KEY}`, {
+    //const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${process.env.GEMINI_API_KEY}`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
